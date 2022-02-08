dcf905b46cf32d913d2710dbf87b74ef7df7bd1c
Gafur Iusupaliev <igafurw@gmail.com>
2022-02-08 15:17:26 -0500

    Clean up and testing auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const users = [
+  { username: "omega", password: "lul" },
+  { username: "kappa", password: "pride" },
+];
+
+export default NextAuth({
+  // Configure one or more authentication providers
+
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const toFind = users.find(
+          (user) =>
+            user.username == credentials.username &&
+            user.password == credentials.password
+        );
+
+        if (toFind) {
+          // Any object returned will be saved in `user` property of the JWT
+          return { name: `${toFind.password} ${toFind.username}` };
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+});
